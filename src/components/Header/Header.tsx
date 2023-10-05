247d99937cb84bb2802c68746cfdb87239efff2a
Daniil Beniash <daniilbeniash97@gmail.com>
2023-10-05 13:49:49 +0300

    feat: header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.scss';
+import { Link } from 'react-router-dom';
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <Link to='/'>
+                <h1 className='header__title'>To Do List</h1>
+            </Link>
+            <div>
+                <div>
+                    <Link to='/signin' className='header__link'>
+                        <img width="35" height="35" src="https://img.icons8.com/ios-filled/50/guest-male--v1.png" alt="guest-male--v1"/>
+                    </Link>
+                </div>
+            </div>
+        </header>    
+    )
+}
